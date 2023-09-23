2ec052bc73b4bfc14c31d2b7286be6e7945dc0bb
Hasan Ahmed <hasahmed@umich.edu>
2023-09-23 05:10:02 -0400

    updated bounds for input validation

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -5,11 +5,11 @@ class Rectangle():
     def __str__(self):
         return f'A rectangle with width {self.width} and height {self.height}'
     def area(self):
-        if self.height < 0 or self.width < 0:
+        if self.height <= 0 or self.width <= 0:
             return "Invalid input"
         return self.width * self.height
     def perimeter(self):
-        if self.height < 0 or self.width < 0:
+        if self.height <= 0 or self.width <= 0:
             return "Invalid input"
         return (self.width * 2) + (self.height * 2)
     
